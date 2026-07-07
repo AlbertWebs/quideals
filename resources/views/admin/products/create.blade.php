cb8d052b946e8e4863343814203d492c35b39785
AlbertWebs <albertmuhatia@gmail.com>
2026-07-07 10:06:41 +0300

    ..

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -196,7 +196,7 @@
                     </button>
                 </div>
                 <div id="ckeditor-container">
-                    <textarea id="description" name="description" rows="10" required
+                    <textarea id="description" name="description" rows="10"
                               class="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                               placeholder="Enter product description (HTML allowed)...">{{ old('description') }}</textarea>
                 </div>
